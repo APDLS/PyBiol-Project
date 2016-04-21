86ac52157af335df3530fde79fe65eb7aa615aeb
APDLS <alexander.douglass@ucdconnect.ie>
2016-04-21 18:33:59 +0100

    Update Assembly_Test.py

--- Assembly_Test.py
+++ Assembly_Test.py
@@ -3,17 +3,10 @@ import subprocess
 from subprocess import call
 print "Input Genome gff File"
 A=raw_input()
-print "Input Reference Genes FASTA File"
-B=raw_input()
 
 #Open Genome File
 f=open(A)
 g=f.readlines()
-f.close()
-
-#Open Reference File
-f=open(B)
-h=f.read()
 f.close()
 
 #Extract regions near assembly gaps
@@ -24,33 +17,52 @@ for i in g:
 	x=x+1
 	if ">" in g[x]:
 		Genome=True
-	while Genome==True and x<len(g):
+	if Genome==True:
 		Sequence=Sequence+str(g[x])
+
+x=0
+y=0
 h=open("Duplicated_Genes", "w")
 for i in g:
-	if "assembly_gap" in i:
-		coords1=int(re.findall("\d+", i))
-		coords2=re.findall("..\d+",i)
-		coords2=int(coords2.replace("..", ""))
-		gap1=g[coords1-1000:coords1]
-		gap2=g[coords2:coords2+1000]
-		f=open("temp_query.fasta", "w")
-		f.write(str(gap1))
-		f.close()
+	x=x+1
+	while i="n":
+		y=y+1
+		if y==10:
+			gap1=g[(x-1010):(x-10)]
+			Gap=True
+			x=x+1
+		if g[x+1]!="n" and Gap=True:
+			gap2=g[x:x+1000]
+	f=open("temp_query.fasta", "w")
+	f.write(str(gap1))
+	f.close()
+	if Gap==True:
 		call(["blastn", "-query", "temp_query.fasta", "-db", A, "-outfmt", "10", "-o", "Duplicates1.csv", "-m", "8"])
 		f=open("temp_query.fasta", "w")
 		f.write(str(gap2))
 		f.close()
 		call(["blastn", "-query", "temp_query.fasta", "-db", A, "-outfmt", "10", "-o", "Duplicates2.csv", "-m", "8"])
 		found = "no"
-		with open("Duplicates.csv", "rb") as f:
+		with open("Duplicates1.csv", "rb") as f:
 			f.next()			
 			mycsv = csv.reader(f)
 			for row in mycsv:
 				text = row[2]
-				if float(text) < 1e-200:
+				if float(text) < 99:
 				print row
 				found = "yes"
 			if found == "yes":
-			h.write(i)
-			h.write("\n")
+				h.write(i)
+				h.write("\n")
+		with open("Duplicates2.csv", "rb") as f:
+			f.next()			
+			mycsv = csv.reader(f)
+			for row in mycsv:
+				text = row[2]
+				if float(text) < 99:
+				print row
+				found = "yes"
+			if found == "yes":
+				h.write(i)
+				h.write("\n")
+		Gap=False
