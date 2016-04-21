6f2b724f42c893138eebf8d6c45c73aa8eb8da63
APDLS <alexander.douglass@ucdconnect.ie>
2016-04-21 19:07:33 +0100

    Update Assembly_Test.py

--- Assembly_Test.py
+++ Assembly_Test.py
@@ -9,30 +9,38 @@ f=open(A)
 g=f.readlines()
 f.close()
 
-#Extract regions near assembly gaps
+#Extract Genome Sequence
 x=0
 Genome=False
 Sequence=""
 for i in g:
 	x=x+1
-	if ">" in g[x]:
-		Genome=True
-	if Genome==True:
-		Sequence=Sequence+str(g[x])
+	while x<len(g):
+		if ">" in g[x]:
+			Genome=True
+		if Genome==True:
+			Sequence=Sequence+str(g[x])
 
+#Extract regions near assembly gaps, regions of 10+ Ns
 x=0
 y=0
 h=open("Duplicated_Genes", "w")
-for i in g:
-	x=x+1
-	while i="n":
+while x<len(Sequence):
+	if Sequence[x]=="N":
 		y=y+1
+		x=x+1
 		if y==10:
-			gap1=g[(x-1010):(x-10)]
+			print "N"
+			gap1=Sequence[x-1010:x-10]
 			Gap=True
 			x=x+1
-		if g[x+1]!="n" and Gap=True:
-			gap2=g[x:x+1000]
+			if Sequence[x+1]!="N" and Gap==True:
+				gap2=Sequence[x:x+1000]
+			else:
+				x=x+1
+	x=x+1
+	y=0
+#Run BLAST regions to either side of the assembly gap
 	f=open("temp_query.fasta", "w")
 	f.write(str(gap1))
 	f.close()
@@ -43,6 +51,7 @@ for i in g:
 		f.close()
 		call(["blastn", "-query", "temp_query.fasta", "-db", A, "-outfmt", "10", "-o", "Duplicates2.csv", "-m", "8"])
 		found = "no"
+#Take hits of over 99% sequence identity from the results files, excluding the first line.		
 		with open("Duplicates1.csv", "rb") as f:
 			f.next()			
 			mycsv = csv.reader(f)
